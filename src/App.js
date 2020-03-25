42c6f67de28e952df8951b1c51a5641b43aea928
David White <dwhite0627@gmail.com>
2020-03-24 22:24:21 -0400

    Add BrowserRoute to the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.scss';
 
+// Client side routing
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Component imports
+import MasterRouter from './components/MasterRouter';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Anywhere Fitness</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Anywhere Fitness</h1>
+        <MasterRouter />
+      </div>
+    </Router>
   );
 }
 
