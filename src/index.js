603a2c4fa1bf2f3f69725c653fded8b3d583fea9
David White <dwhite0627@gmail.com>
2020-03-25 20:36:38 -0400

    Add redux-logger dependency

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 // Reducer imports
 import { fitnessReducer } from './reducers/fitnessReducer';
 
 // The redux store
-const store = createStore(fitnessReducer, applyMiddleware(thunk));
+const store = createStore(fitnessReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
