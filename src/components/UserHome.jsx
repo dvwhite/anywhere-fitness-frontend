219c489bf72562c847416de7f83c104fbbe2bb06
David White <dwhite0627@gmail.com>
2020-03-27 21:52:33 -0400

    Add Header to individual components rather than across the entire app

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 // Redux imports
 import { connect } from 'react-redux';
 
+// Component imports
+import Header from "./Header";
+
 // Styled components
 import styled from 'styled-components';
 
 const UserHome = props => {
   return (
     <div>
+      <Header />
       <h1>{props.message}</h1>
     </div>
   );
