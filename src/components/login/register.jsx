cb0b081dadbe4422e054ac2af71c2b373c71301e
David White <dwhite0627@gmail.com>
2021-03-18 20:19:57 -0400

    Update the base URLs on the component

--- src/components/login/register.jsx
+++ src/components/login/register.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import loginImg from "../../login.png";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import loginImg from '../../login.png';
+import { useHistory } from 'react-router-dom';
 
 // Redux imports
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 
 // Actions
-import { register } from "../../actions/actions";
+import { register } from '../../actions/actions';
 
-export const Register = props => {
+export const Register = (props) => {
   // Local State
   const [input, setInput] = useState({
-    username: "",
-    password: "",
-    firstName: "",
-    lastName: "",
-    email: "",
-    roleId: 1
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    roleId: 1,
   });
 
   // Utility hooks
@@ -24,14 +24,14 @@ export const Register = props => {
   const dispatch = useDispatch();
 
   // Event handlers
-  const handleChange = e => {
+  const handleChange = (e) => {
     e.preventDefault();
     setInput({ ...input, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("clicking submit")
+    console.log(input);
     dispatch(
       register(
         {
@@ -40,7 +40,7 @@ export const Register = props => {
           firstName: input.firstName,
           lastName: input.lastName,
           email: input.email,
-          roleId: input.role
+          roleId: input.role,
         },
         history
       )
@@ -48,78 +48,79 @@ export const Register = props => {
   };
 
   return (
-    <div className="base-container" ref={props.containerRef}>
-      <div className="header">Register</div>
-      <div className="content">
-        <div className="image">
-          <img src={loginImg} alt='register lock symbol'/>
+    <div className='base-container' ref={props.containerRef}>
+      <div className='header'>Register</div>
+      <div className='content'>
+        <div className='image'>
+          <img src={loginImg} alt='register lock symbol' />
         </div>
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username: </label>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <label htmlFor='username'>Username: </label>
             <input
-              type="text"
-              name="username"
-              placeholder="Please enter your username"
+              type='text'
+              name='username'
+              placeholder='Please enter your username'
               value={input.username}
               onChange={handleChange}
             />
           </div>
 
-          <div className="form-group">
-            <label htmlFor="password">Password: </label>
+          <div className='form-group'>
+            <label htmlFor='password'>Password: </label>
             <input
-              type="text"
-              name="password"
-              placeholder="Please enter your password"
+              type='text'
+              name='password'
+              placeholder='Please enter your password'
               value={input.password}
               onChange={handleChange}
             />
           </div>
 
-          <div className="form-group">
-            <label htmlFor="firstname">First Name: </label>
+          <div className='form-group'>
+            <label htmlFor='firstname'>First Name: </label>
             <input
-              type="text"
-              name="firstName"
-              placeholder="Please enter your first name"
+              type='text'
+              name='firstName'
+              placeholder='Please enter your first name'
               value={input.firstName}
               onChange={handleChange}
             />
           </div>
 
-          <div className="form-group">
-            <label htmlFor="lastname">Last Name: </label>
+          <div className='form-group'>
+            <label htmlFor='lastname'>Last Name: </label>
             <input
-              type="text"
-              name="lastName"
-              placeholder="Please enter your last name"
+              type='text'
+              name='lastName'
+              placeholder='Please enter your last name'
               value={input.lastName}
               onChange={handleChange}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="email">Email: </label>
+          <div className='form-group'>
+            <label htmlFor='email'>Email: </label>
             <input
-              type="text"
-              name="email"
-              placeholder="Please enter your email"
+              type='text'
+              name='email'
+              placeholder='Please enter your email'
               value={input.email}
               onChange={handleChange}
             />
           </div>
 
-          <div className="form-group">
-            <label htmlFor="role">Register As: </label>
-            <select onChange={handleChange} name="role">
+          <div className='form-group'>
+            <label htmlFor='role'>Register As: </label>
+            <select onChange={handleChange} name='role'>
+              <option>Select</option>
               <option value={1}>Client</option>
               <option value={2}>Instructor</option>
             </select>
           </div>
         </form>
       </div>
-      <div className="footer">
-        <button type="submit" className="btn" onClick={handleSubmit}>
+      <div className='footer'>
+        <button type='submit' className='btn' onClick={handleSubmit}>
           Register
         </button>
       </div>
